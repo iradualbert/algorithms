7e2e8ddee94173067b22c595a532dc23e77808f3
iradualbert <albert.iradukunda@yahoo.com>
2021-03-14 13:26:08 +0300

    first commit

--- binary_search/main.py
+++ binary_search/main.py
@@ -1,3 +1,24 @@
+arr = [2,3,5,8,10, 11,15, 18, 20, 21, 23]
 
-arr = [2,3,5,8,10, 11,15, 18, 20, 21, 23, 25]
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+    
+    while(left <= right):
+        midpoint = (left + right) // 2
+        
+        if(arr[midpoint] == target):
+            return midpoint
+        
+        elif arr[midpoint] > target:
+            right = midpoint - 1
+            
+        else:
+            left = midpoint + 1
+            
+    return -1
+
+print(binary_search(arr, 5)) #2
+print(binary_search(arr, 9)) # -1
+print(binary_search(arr, 15)) # 6
 
